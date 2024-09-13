aaa965d064ee0002d25254d6e610d99c2186aa53
Lmakinaa <jaija.ismail@gmail.com>
2024-09-13 23:18:14 +0100

    hmm

--- ftp/c-entry.sh
+++ ftp/c-entry.sh
@@ -4,7 +4,14 @@ adduser --home /wp "test" --disabled-password && printf "admin\nadmin" | passwd 
 
 chown test:test /wp;
 
-echo "write_enable=YES\npasv_enable=YES\nchroot_local_user=YES\npasv_min_port=40000\npasv_max_port=50000\npam_service_name=ftp\nallow_writeable_chroot=YES" >> /etc/vsftpd.conf;
+echo "
+write_enable=YES
+pasv_enable=YES
+pasv_min_port=40000
+pasv_max_port=50000
+pam_service_name=ftp
+allow_writeable_chroot=YES
+" >> /etc/vsftpd.conf;
 
 echo "test" > /etc/ftpusers;
 
