7d7146b0d0b029cd65571deaa52faf0c2752e743
Lmakinaa <jaija.ismail@gmail.com>
2024-09-13 19:20:01 +0100

    hm

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -21,13 +21,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', '<DB_NAME>' );
+define( 'DB_NAME', 'wordpress' );
 
 /** Database username */
-define( 'DB_USER', '<DB_USER>' );
+define( 'DB_USER', 'ismail' );
 
 /** Database password */
-define( 'DB_PASSWORD', '<DB_PASS>' );
+define( 'DB_PASSWORD', 'admin' );
 
 /** Database hostname */
 define( 'DB_HOST', 'mariadb' );
