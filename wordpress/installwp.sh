788197a4f537a36cfd1d80d47510265be02fd3a4
Lmakinaa <jaija.ismail@gmail.com>
2024-09-13 17:47:47 +0100

    hmm

--- wordpress/installwp.sh
+++ wordpress/installwp.sh
@@ -9,9 +9,9 @@ chmod +x ./wp-cli.phar && \
 
 cp /root/wp-config.php ./wp-config.php && chown -R www-data /var/www/html/wp-content ;
 
-$sed "s/<DB_NAME>/$DB_NAME/" ./wp-config.php;
-$sed "s/<DB_USER>/$DB_USER/" ./wp-config.php;
-$sed "s/<DB_PASS>/$DB_PASS/" ./wp-config.php;
+sed "s/<DB_NAME>/$DB_NAME/" ./wp-config.php;
+sed "s/<DB_USER>/$DB_USER/" ./wp-config.php;
+sed "s/<DB_PASS>/$DB_PASS/" ./wp-config.php;
 
 
 ./wp-cli.phar core install --path=/var/www/html --url=$WP_URL --title=inception --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_MAIL --allow-root && \
