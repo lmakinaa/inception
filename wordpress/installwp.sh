d832d3ba164e646ac213a5f482b76493945d45d3
Lmakinaa <jaija.ismail@gmail.com>
2024-09-13 23:54:03 +0100

    hmm

--- wordpress/installwp.sh
+++ wordpress/installwp.sh
@@ -6,11 +6,11 @@ curl https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -
 chmod +x ./wp-cli.phar && \
 ./wp-cli.phar core download --path=/var/www/html --allow-root;
 
-cp /root/wp-config.php /var/www/html
+cp /root/wp-config.php /var/www/html/wp-config.php;
 
-sed -i "s/<DB_NAME>/$DB_NAME/" /root/wp-config.php;
-sed -i "s/<DB_USER>/$DB_USER/" /root/wp-config.php;
-sed -i "s/<DB_PASS>/$DB_PASS/" /root/wp-config.php;
+sed -i "s/<DB_NAME>/$DB_NAME/" /var/www/html/wp-config.php;
+sed -i "s/<DB_USER>/$DB_USER/" /var/www/html/wp-config.php;
+sed -i "s/<DB_PASS>/$DB_PASS/" /var/www/html/wp-config.php;
 
 ./wp-cli.phar core install --path=/var/www/html --url=$WP_URL --title=inception --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_MAIL --allow-root && \
 ./wp-cli.phar user create $WP_USER $WP_MAIL --role=editor --user_pass=$WP_PASS --path=/var/www/html --allow-root && \
