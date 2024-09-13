398d36b790574ca7f89fcde9a6231b76a25c4fe6
Lmakinaa <jaija.ismail@gmail.com>
2024-09-13 23:40:45 +0100

    this is good

--- mariadb/init.sql
+++
@@ -1,9 +0,0 @@
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'Ofppt@123';
-
-CREATE DATABASE IF NOT EXISTS wordpress;
-
--- Create a new user and grant privileges
--- % is a wildcard that allow the user to connect to the db from any host
-CREATE USER 'ismail'@'%' IDENTIFIED BY 'admin';
-GRANT ALL PRIVILEGES ON *.* TO 'ismail'@'%';
-FLUSH PRIVILEGES;
