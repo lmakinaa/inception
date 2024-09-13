2d8c45cbecf337caa2761e7a2f34e434963de5f9
Lmakinaa <jaija.ismail@gmail.com>
2024-09-13 23:40:45 +0100

    this is good

--- mariadb/c-entry.sh
+++ mariadb/c-entry.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
+echo "
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';
+
+CREATE DATABASE IF NOT EXISTS wordpress;
+
+-- Create a new user and grant privileges
+-- % is a wildcard that allow the user to connect to the db from any host
+CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
+GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%';
+FLUSH PRIVILEGES;
+" > /etc/mysql/init.sql;
+
 mysql_install_db;
 mysqld_safe;
--- wordpress/installwp.sh
+++ wordpress/installwp.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
 cd /var/www/html;
-
-echo "listen = 9000" >> /etc/php/8.2/fpm/pool.d/www.conf;
 rm -rf *;
 curl https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -o ./wp-cli.phar && \
 chmod +x ./wp-cli.phar && \
-./wp-cli.phar core download --path=/var/www/html --allow-root && \
-./wp-cli.phar config create --path=/var/www/html --dbname=wordpress --dbuser=ismail --dbpass=admin --dbhost=mariadb --allow-root && \
-./wp-cli.phar core install --path=/var/www/html --url=ijaija.42.fr --title=inception --admin_user=admin --admin_password=admin --admin_email=ijaija@student.1337.ma --allow-root && \
+./wp-cli.phar core download --path=/var/www/html --allow-root;
+
+sed -i "s/<DB_NAME>/$DB_NAME/" /root/wp-config.php;
+sed -i "s/<DB_USER>/$DB_USER/" /root/wp-config.php;
+sed -i "s/<DB_PASS>/$DB_PASS/" /root/wp-config.php;
+
+./wp-cli.phar core install --path=/var/www/html --url=$WP_URL --title=inception --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_MAIL --allow-root && \
+./wp-cli.phar user create $WP_USER $WP_USER_MAIL --role=subscriber --user_pass=$WP_PASS --path=/var/www/html --allow-root && \
+
 cp /root/wp-config.php ./wp-config.php && chown -R www-data /var/www/html/wp-content && php-fpm8.2 -F
