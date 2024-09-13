6d8a5c6b4b38fcde8e0456bb6b84496abae395d2
Ismail Jaija <ijaija@e3r1p5.1337.ma>
2024-09-13 15:41:03 +0100

    fixed env file

--- wordpress/installwp.sh
+++ wordpress/installwp.sh
@@ -7,6 +7,6 @@ chmod +x ./wp-cli.phar && \
 ./wp-cli.phar core download --path=/var/www/html --allow-root && \
 #./wp-cli.phar config create --path=/var/www/html --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=mariadb --allow-root && \
 ./wp-cli.phar core install --path=/var/www/html --url=$WP_URL --title=inception --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_MAIL --allow-root && \
+cp /root/wp-config.php ./wp-config.php && chown -R www-data /var/www/html/wp-content && php-fpm8.2 -F
 ./wp-cli.phar user create --path=/var/www/html --user_login=$WP_USER --user_pass=$WP_PASS --user_email=$WP_MAIL --role=subscriber --allow-root && \
 ./wp-cli.phar plugin install redis-cache --activate --allow-root
-cp /root/wp-config.php ./wp-config.php && chown -R www-data /var/www/html/wp-content && php-fpm8.2 -F
