266dabab95f97206d4a181f8eb865ec59c6be5e2
Lmakinaa <jaija.ismail@gmail.com>
2024-09-19 18:20:24 +0100

    changing default grafana admin creds

--- srcs/requirements/bonus/grafana/tools/c-entry.sh
+++ srcs/requirements/bonus/grafana/tools/c-entry.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sed -i "s/admin_user = admin/admin_user = $GRAFANA_ADMIN_USER/" conf/defaults.ini;
-sed -i "s/admin_password = admin/admin_password = $GRAFANA_ADMIN_PASS/" conf/defaults.ini;
+sed -i "s/admin_user = admin/admin_user = $GRAFANA_ADMIN_USER/" /grafana-v11.2.0/conf/defaults.ini;
+sed -i "s/admin_password = admin/admin_password = $GRAFANA_ADMIN_PASS/" /grafana-v11.2.0/conf/defaults.ini;
 
 /grafana-v11.2.0/bin/grafana server > /dev/null
