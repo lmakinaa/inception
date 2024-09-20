2fc337aaec2e5f896449c80cf45b8486bc76eb42
Lmakinaa <jaija.ismail@gmail.com>
2024-09-20 01:51:21 +0100

    final touches

--- srcs/requirements/mariadb/tools/c-entry.sh
+++ srcs/requirements/mariadb/tools/c-entry.sh
@@ -5,9 +5,7 @@ ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';
 
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
 
--- Create a new user and grant privileges
--- % is a wildcard that allow the user to connect to the db from any host
-CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
+CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
 GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%';
 FLUSH PRIVILEGES;
 " > /etc/mysql/init.sql;
